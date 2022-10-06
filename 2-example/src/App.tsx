5b009afa4f129ffa3135cead9f5509ac46600a48
huytv <tranvanhuy0122@gmail.com>
2022-10-06 13:45:03 +0700

    baitap9/10

--- 2-example/src/App.tsx
+++ 2-example/src/App.tsx
@@ -5,37 +5,36 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <div id="header">The Pulpit Rock</div>
-      <div id="main">
-        <div className="row">
-          <div className="col">
-            <ul>
-              <li>The Driver</li>
-              <li>The Walk</li>
-              <li>The Return</li>
-              <li>The End</li>
-            </ul>
-          </div>
-          <div className="col-6">
-            <h1>The Walk</h1>
-            <p>The Walk to the Pulpit Rock will take your</p>
-            <img src="https://s27363.pcdn.co/wp-content/uploads/2019/09/Pulpit-Rock.jpg.optimal.jpg" />
-          </div>
-          <div className="col">
-            <div className="intro">
-              <h1>What?</h1>
-              <p>The Walk to the Pulpit Rock will take your</p>
-              <h1>Where?</h1>
-              <p>The Walk to the Pulpit Rock will take your</p>
-              <h1>Price?</h1>
-              <p>The Walk is free</p>
-            </div>
-          </div>
+      <div className="bg-ccc text-white fs-1 text-center">The Pulpit Rock</div>
+      <div className="row pt-5">
+        <div className="col-md-4">
+          <p className="border-bottom py-4 ps-3">The Driver</p>
+          <p className="border-bottom py-4 ps-3">The Driver</p>
+          <p className="border-bottom py-4 ps-3">The Driver</p>
+          <p className="border-bottom py-4 ps-3">The Driver</p>
+        </div>
+        <div className="col-md-4">
+          <h1>The Walk</h1>
+          <p>
+            The Walk to the Pulpit Rock will take your to the Pulpit Rock will
+            take your to the Pulpit Rock will take your
+          </p>
+          <img
+            className="w-100"
+            src="https://s27363.pcdn.co/wp-content/uploads/2019/09/Pulpit-Rock.jpg.optimal.jpg"
+            alt=""
+          />
+        </div>
+        <div className="col-md-4 bg-secondary h-50">
+          <h1>What?</h1>
+          <p>The Walk to the Pulpit Rock will take your</p>
+          <h1>Where?</h1>
+          <p>The Walk to the Pulpit Rock will take your</p>
+          <h1>Price?</h1>
+          <p>The Walk is free</p>
         </div>
       </div>
-      <div id="footer">
-        <h2>Footer</h2>
-      </div>
+      <div className="bg-ccc text-white fs-1 text-center mb-0">Footer</div>
     </div>
   );
 }

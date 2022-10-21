dd7435722840404b298873bfd0ebfada0011754b
huytv <tranvanhuy0122@gmail.com>
2022-10-21 21:26:09 +0700

    update homework

--- 2-example/src/App.tsx
+++ 2-example/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import FormTest from "./Components/Bt/FormTest";
+import Form01 from "./Homeworks/Section06/Form01";
 // import Header from "./Components/Header";
 // import Footer from "./Components/Footer";
 // import Basic01 from "./Homeworks/Section02/Basic01";
@@ -20,7 +21,7 @@ import FormTest from "./Components/Bt/FormTest";
 function App() {
   return (
     <>
-      <FormTest />
+      <Form01 />
     </>
   );
 }
